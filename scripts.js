aecdc21a93b09efbc0e70f0bb2bdb22fc1880338
Divya Gandhi <divyagandhi@gmail.com>
2023-09-09 23:14:05 +0530

    update

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@ const timeSelect = document.querySelector('.time select');
 const eventSelect = document.querySelector('.event select');
 
 signInButton.addEventListener('click', function () {
-    // Fetch data from form fields
     const username = usernameInput.value;
     const password = passwordInput.value;
     const email = emailInput.value;
@@ -16,15 +15,12 @@ signInButton.addEventListener('click', function () {
     const time = timeSelect.value;
     const event = eventSelect.value;
 
-    // Check if username is empty
     if (username === '') {
         alert('Please enter a username');
         return;
     }
 
-    // Check the password
     if (password === '2710') {
-        // Password is correct
     } else if (password === '') {
         alert('Enter password');
         return;
@@ -32,21 +28,10 @@ signInButton.addEventListener('click', function () {
         alert('Enter valid password');
         return;
     }
-
-    // Check if the email is valid
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        alert('Enter a valid email');
+    if(email==''){
+        alert("Enter a valid email")
         return;
     }
-
-    // Assuming the form is valid, you can use the fetched data as needed
-    console.log('Username: ' + username);
-    console.log('Selected Date: ' + date);
-    console.log('Selected Time: ' + time);
-    console.log('Selected Event: ' + event);
-
-    // Perform further actions here
     alert('Form verified successfully!');
 });
 
